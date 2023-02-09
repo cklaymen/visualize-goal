119481efb9ccefcf965168a341ad2caa55545281
Ryszard Plewnia <hello@erys.io>
2023-02-09 23:55:33 +0100

    Calculate worked hours as float number

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Box } from "@mui/material";
 
 import ListAwards from "./components/awards/ListAwards";
 import Actions from "./components/actions";
+import Income from "./components/income/Income";
 
 function App() {
   return (
-    <Box sx={{ display: "grid", height: 1, gridTemplateRows: "1fr auto" }}>
+    <Box sx={{ display: "grid", height: 1, gridTemplateRows: "auto 1fr auto" }}>
+      <Income />
       <ListAwards />
       <Actions />
     </Box>
--- src/components/income/Income/Income.tsx
+++ src/components/income/Income/Income.tsx
@@ -1,5 +1,18 @@
 import { Box } from "@mui/material";
+import dayjs from "dayjs";
 
-const Income: React.FC = () => <Box>INCOME</Box>;
+import { useStore } from "../../../store";
+
+import getWorkedHours from "./helpers/getWorkedHours";
+
+const Income: React.FC = () => {
+    const incomeSettings = useStore(value => value.incomeSettings);
+    if (!incomeSettings) {
+        return <Box>Brak ustawień</Box>
+    }
+    const workedHours = getWorkedHours(dayjs(incomeSettings.firstDayDate, "YYYY-MM-DD"), incomeSettings.startTime, incomeSettings.endTime)
+
+    return <Box>{workedHours}{JSON.stringify(incomeSettings)}</Box>;
+}
 
 export default Income;
